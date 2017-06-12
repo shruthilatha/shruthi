acd1766f62c8ee2b06d457d910a5311b8181859a
shruthilatha <shruthilatha1995@gmail.com>
2017-06-12 23:33:49 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+    public static void main(String a[])
+    {
+        Scanner s=new Scanner(System.in);
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Hello"+" ");
+        }
+    }
+}
