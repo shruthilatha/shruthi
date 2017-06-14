cbce772066377108c651be6763d79d0ea15991a7
shruthilatha <shruthilatha1995@gmail.com>
2017-06-14 18:51:12 +0530

    Create enum.java

---
+++ enum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class enum
+{
+	public static void main (String[] args)
+  {
+		Scanner s=new Scanner(System.in);
+		for(int i=10;i<=20;i++)
+		{
+		    if(i%2==0)
+		    {
+		        System.out.println(i);
+		    }
+		 }
+	}
+}
