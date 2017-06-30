004b72f1e24b63f1c61485a86854944bcc378392
shruthilatha <shruthilatha1995@gmail.com>
2017-06-30 11:29:31 +0530

    Create key.java

---
+++ key.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class key {
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.nextLine();
+			if(s1.equals("Q"))
+		{
+		    System.exit(0);
+		}
+	}
+}
